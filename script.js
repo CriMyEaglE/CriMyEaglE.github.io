00db8eb1945b5b4da1e621cc37998b1dfeed4e20
CriMyEaglE <kulikovisfsb@gmail.com>
2022-08-04 17:44:29 +0700

    fix: edit, add кнопки и функционал их popup, создание карточек функцией

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-const editButton = document.querySelector('#editButton');
-editButton.addEventListener('click', function () {
-  document.querySelector('#editFormPopup').classList.add('popup_opened');
-  document.querySelector('#editFormName').value = document.querySelector('.profile__name').textContent;
-  document.querySelector('#editFormJob').value = document.querySelector('.profile__description').textContent;
-  timerFade = setTimeout(() => document.querySelector('.popup_opened').style.opacity = 1, 50);
+const editForm = document.querySelector('#editFormPopup');
+
+const editButton = document.querySelector('.profile__edit-button');
+
+const profileName = document.querySelector('.profile__name');
+
+const profileJob = document.querySelector('.profile__description');
+
+const addForm = document.querySelector('#addFormPopup');
+
+const addButton = document.querySelector('#addButton');
+
+function closePopup(popup) {
+  const closeButton = popup.querySelector('.edit-form__close-button');
+  popup.style.opacity = 0;
+  setTimeout(function() {popup.classList.add('popup_hidden')}, 500)
+}
+
+function openPopup(popup) {
+  const closeButton = popup.querySelector('.edit-form__close-button');
+  popup.style.opacity = 1;
+  closeButton.addEventListener('click', () => {
+    closePopup(popup);
+  })
+  popup.classList.remove('popup_hidden');
+}
+
+editButton.addEventListener('click',function() {
+  openPopup(editForm);
+  editForm.querySelector('#inputName').value = profileName.textContent;
+  editForm.querySelector('#inputJob').value = profileJob.textContent;
 });
 
-const closeButton = document.querySelector('#closeButton');
-closeButton.addEventListener('click', function () {
-  let timerFade = setTimeout(() => document.querySelector('.popup_opened').style.opacity = 0, 50);
-  let timerClosed = setTimeout(() => document.querySelector('#editFormPopup').classList.remove('popup_opened'), 500);
+addButton.addEventListener('click', function() {
+  openPopup(addForm);
+  addForm.querySelector('#photoName').value = '';
+  addForm.querySelector('#photoLink').value = '';
 });
 
-const editFormSaveButton = document.querySelector('#editFormSaveButton');
-editFormSaveButton.addEventListener('click', function () {
-  document.querySelector('.profile__name').textContent = document.querySelector('#editFormName').value;
-  document.querySelector('.profile__description').textContent = document.querySelector('#editFormJob').value;
-  let timerFade = setTimeout(() => document.querySelector('.popup_opened').style.opacity = 0, 50);
-  let timerClosed = setTimeout(() => document.querySelector('#editFormPopup').classList.remove('popup_opened'), 500);
-});
+function formSubmitHandlerChangeProfile (evt) {
+  evt.preventDefault();
+  const newName = editForm.querySelector('#inputName').value;
+  const newJob = editForm.querySelector('#inputJob').value;
+  const oldName = document.querySelector('.profile__name');
+  const oldJob = document.querySelector('.profile__description');
+  oldName.textContent = newName;
+  oldJob.textContent = newJob;
+  closePopup(editForm);
+}
+editForm.addEventListener('submit', formSubmitHandlerChangeProfile); 
 
-const likeButton = document.querySelector('#likeButton');
-likeButton.addEventListener('click', function () {
-  document.querySelector('#likeButton').classList.toggle('card__like-button_active');
-})
 
 const initialCards = [
   {
@@ -52,43 +77,28 @@ const initialCards = [
   }
 ];
 
-for (let i = 0; i < initialCards.length; i++) {
+function createCard(photoName, photoLink) {
+
   const cardTemplate = document.querySelector('#card').content;
   const cards = document.querySelector('.cards');
   const newCard = cardTemplate.querySelector('.card').cloneNode(true);
-  newCard.querySelector('.card__image').src = initialCards[i].link;
-  newCard.querySelector('.card__image').setAttribute('alt', initialCards[i].name)
-  newCard.querySelector('.card__title').textContent = initialCards[i].name;
-  cards.append(newCard);
+
+  newCard.querySelector('.card__title').textContent = photoName;
+  newCard.querySelector('.card__image').setAttribute('alt', photoName)
+  newCard.querySelector('.card__image').src = photoLink;
+
+  cards.prepend(newCard);
 }
 
-const addForm = document.querySelector('#addFormPopup');
-const addButton = document.querySelector('#addButton');
+for (let i = 0; i < initialCards.length; i++) {
+  createCard(initialCards[i].name,initialCards[i].link);
+}
 
-addButton.addEventListener('click', function() {
-  addForm.classList.add('popup_opened');
-  timerFade = setTimeout(() => document.querySelector('.popup_opened').style.opacity = 1, 50);
-  addForm.querySelector('#photoLink').value ='';
-  addForm.querySelector('#photoName').value ='';
-});
-
-const addFormCloseButton = addForm.querySelector('#closeButton');
-addFormCloseButton.addEventListener('click', function() {  
-  let timerFade = setTimeout(() => document.querySelector('.popup_opened').style.opacity = 0, 50);
-  let timerClosed = setTimeout(() => addForm.classList.remove('popup_opened'), 500);
-});
-
-const addFormSaveButton = addForm.querySelector('#saveButton');
-addFormSaveButton.addEventListener('click', function() {
-
-  const cardTemplate = document.querySelector('#card').content;
-  const cards = document.querySelector('.cards');
-  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
-  newCard.querySelector('.card__image').src = addForm.querySelector('#photoLink').value;
-  newCard.querySelector('.card__image').setAttribute('alt', addForm.querySelector('#photoName').value);
-  newCard.querySelector('.card__title').textContent = addForm.querySelector('#photoName').value;
-  cards.prepend(newCard);
-  
-  let timerFade = setTimeout(() => document.querySelector('.popup_opened').style.opacity = 0, 50);
-  let timerClosed = setTimeout(() => addForm.classList.remove('popup_opened'), 500);
-});
+function formSubmitHandlerNewCard (evt) {
+  evt.preventDefault();
+  const newPhotoName = addForm.querySelector('#photoName').value;
+  const newPhotoLink = addForm.querySelector('#photoLink').value;
+  createCard(newPhotoName, newPhotoLink);
+  closePopup(addForm);
+}
+addForm.addEventListener('submit', formSubmitHandlerNewCard); 
